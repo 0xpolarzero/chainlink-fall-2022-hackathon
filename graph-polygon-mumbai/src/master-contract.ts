c8ecae1b180c851236138189484ff9bc9a259a73
polar0 <23r0eth@gmail.com>
2022-10-18 10:09:14 +0200

    Add graph module

---
+++ graph-polygon-mumbai/src/master-contract.ts
@@ -0,0 +1,42 @@
+import { Address } from '@graphprotocol/graph-ts';
+import { ChildContractCreated as ChildContractCreatedEvent } from '../generated/MasterContract/MasterContract';
+import { ChildContractCreated } from '../generated/schema';
+
+export function handleChildContractCreated(
+  event: ChildContractCreatedEvent,
+): void {
+  // It should never happen that the same contract is created twice
+  // But we can't ever be sure enough, so we check if the entity already exists anyway
+  let childContractCreated = ChildContractCreated.load(
+    getIdFromEventParams(
+      event.params._agreementName,
+      event.params._contractAddress,
+    ),
+  );
+
+  if (!childContractCreated) {
+    childContractCreated = new ChildContractCreated(
+      getIdFromEventParams(
+        event.params._agreementName,
+        event.params._contractAddress,
+      ),
+    );
+  }
+
+  childContractCreated.owner = event.params._owner;
+  childContractCreated.contractAddress = event.params._contractAddress;
+  childContractCreated.agreementName = event.params._agreementName;
+  childContractCreated.pdfUri = event.params._pdfUri;
+  childContractCreated.partyNames = event.params._partyNames;
+  childContractCreated.partyTwitterHandles = event.params._partyTwitterHandles;
+  childContractCreated.partyAddresses = event.params._partyAddresses;
+
+  childContractCreated.save();
+}
+
+function getIdFromEventParams(
+  agreementName: String,
+  contractAddress: Address,
+): string {
+  return agreementName + '-' + contractAddress.toHexString();
+}
