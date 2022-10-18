c987ddc17586c9f0c60d9adde13e32f5035371a2
polar0 <23r0eth@gmail.com>
2022-10-18 19:25:45 +0200

    Change folder structure

---
+++ backend/hardhat/deploy/99-update-front-end.js
@@ -0,0 +1,49 @@
+const { ethers, network } = require('hardhat');
+const fs = require('fs');
+
+const frontEndContractsFile = '../../frontend/constants/networkMapping.json';
+const frontEndAbiFolder = '../../frontend/constants/';
+
+module.exports = async () => {
+  if (process.env.UPDATE_FRONT_END) {
+    console.log('Updating front end...');
+    await updateContractAddresses();
+    await updateAbi();
+  }
+};
+
+async function updateContractAddresses() {
+  const masterContract = await ethers.getContract('MasterContract');
+  const chainId = network.config.chainId;
+
+  const contractAddresses = JSON.parse(
+    fs.readFileSync(frontEndContractsFile, 'utf8'),
+  );
+  if (chainId in contractAddresses) {
+    if (
+      !contractAddresses[chainId]['MasterContract'].includes(
+        masterContract.address,
+      )
+    ) {
+      contractAddresses[chainId]['MasterContract'].push(masterContract.address);
+    }
+  } else {
+    contractAddresses[chainId] = {
+      MasterContract: [masterContract.address],
+    };
+  }
+
+  fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses));
+
+  console.log('Front end updated!');
+}
+
+async function updateAbi() {
+  const masterContract = await ethers.getContract('MasterContract');
+  fs.writeFileSync(
+    `${frontEndAbiFolder}MasterContract.json`,
+    masterContract.interface.format(ethers.utils.FormatTypes.json),
+  );
+}
+
+module.exports.tags = ['all', 'frontend'];
