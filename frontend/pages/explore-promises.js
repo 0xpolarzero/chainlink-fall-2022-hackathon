0bc405e10f649afb8b11a49ae4d2370bc5df709a
polar0 <23r0eth@gmail.com>
2022-10-20 23:40:05 +0200

    Show pagination in Explore section and allow to go through the pages

--- frontend/pages/explore-promises.js
+++ frontend/pages/explore-promises.js
@@ -3,14 +3,26 @@ import PromisesCollapse from '../components/PromisesCollapse';
 import PromisesCollapseSkeleton from '../components/PromisesCollapseSkeleton';
 import { GET_CHILD_CONTRACT_CREATED } from '../constants/subgraphQueries';
 import { useQuery } from '@apollo/client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Pagination } from 'antd';
 
 export default function explorePromises({ setActivePage }) {
+  const [shownPage, setShownPage] = useState(1);
+  const [shownPromises, setShownPromises] = useState([]);
   const { data, loading, error } = useQuery(GET_CHILD_CONTRACT_CREATED);
 
   useEffect(() => {
     setActivePage(1);
   }, []);
+
+  useEffect(() => {
+    // Get the shown page and show relevant set of promises, 10 per page
+    if (!!data && !loading && !error) {
+      setShownPromises(
+        data.childContractCreateds.slice((shownPage - 1) * 10, shownPage * 10),
+      );
+    }
+  }, [shownPage, loading]);
 
   if (error) {
     console.log(error);
@@ -29,7 +41,15 @@ export default function explorePromises({ setActivePage }) {
           {loading ? (
             <PromisesCollapseSkeleton arraySize={3} />
           ) : !!data ? (
-            <PromisesCollapse promises={data.childContractCreateds} />
+            <div className='promises-list-wrapper'>
+              <PromisesCollapse promises={shownPromises} />
+              <Pagination
+                simple
+                defaultCurrent={1}
+                total={data.childContractCreateds.length}
+                onChange={(e) => setShownPage(e)}
+              />
+            </div>
           ) : (
             'Seems like there was an error loading this page... Please try refreshing.'
           )}
