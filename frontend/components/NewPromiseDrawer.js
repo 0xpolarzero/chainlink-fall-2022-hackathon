20b27fdb0e5da9ae09646390c081d4a3a761c617
polar0 <23r0eth@gmail.com>
2022-10-22 09:06:07 +0200

    Separate the long Form.List in a new component

--- frontend/components/NewPromiseDrawer.js
+++ frontend/components/NewPromiseDrawer.js
@@ -1,3 +1,4 @@
+import PDFUploader from './PDFUploader';
 import { Input, Tooltip, Form, Drawer, Space, Button } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 import { useAccount } from 'wagmi';
@@ -139,172 +140,171 @@ export default function NewPromiseDrawer({ drawerOpen, setDrawerOpen }) {
             />
           </Form.Item>
 
-          {/* Make sure there is one field added by default */}
-          <Form.List
-            name='parties'
-            initialValue={[{ partyAddress: userAddress }]}
-          >
-            {(fields, { add, remove }) => (
-              <>
-                {fields.map((field, i) => (
-                  <Space
-                    key={field.key}
-                    style={{ display: 'flex', marginBottom: 8 }}
-                    align='center'
-                  >
-                    <Form.Item
-                      {...field}
-                      name={[field.name, 'partyName']}
-                      key={[field.key, 'partyName']}
-                      // Put a label only if it's the first field
-                      label={i === 0 ? 'Name' : null}
-                      rules={[
-                        {
-                          required: true,
-                          message: 'Missing name',
-                        },
-                        {
-                          type: 'string',
-                          message: 'Name must be a string',
-                        },
-                        {
-                          min: 2,
-                          message: 'Name must be at least 2 characters',
-                        },
-                        {
-                          max: 30,
-                          message: 'Role must be less than 15 characters',
-                        },
-                      ]}
-                    >
-                      <Input
-                        placeholder='John'
-                        prefix={<i className='fa-solid fa-user-tag'></i>}
-                        suffix={
-                          <Tooltip title='The name of the participant must be between 2 and 30 characters long.'>
-                            <InfoCircleOutlined
-                              style={{ color: 'rgba(0,0,0,.45)' }}
-                            />
-                          </Tooltip>
-                        }
-                      />
-                    </Form.Item>
-
-                    <Form.Item
-                      {...field}
-                      name={[field.name, 'partyAddress']}
-                      key={[field.key, 'partyAddress']}
-                      label={i === 0 ? 'Ethereum address' : null}
-                      rules={[
-                        {
-                          required: true,
-                          message: 'Missing address',
-                        },
-                        {
-                          type: 'string',
-                          message: 'Address must be a number',
-                        },
-                        // Make sure it is an ethereum address
-                        {
-                          pattern: /^0x[a-fA-F0-9]{40}$/,
-                          min: 42,
-                          max: 42,
-                          message: 'Address must be a valid ethereum address',
-                        },
-                        // Make sure it's not the dead address
-                        {
-                          pattern:
-                            /^(?!.*?(0x0000000000000000000000000000000000000000)).*$/,
-                          message:
-                            'You might want to use a different address...',
-                          warningOnly: true,
-                        },
-                      ]}
-                    >
-                      <Input
-                        placeholder='0xc06...EF'
-                        prefix={<i className='fa-brands fa-ethereum'></i>}
-                        suffix={
-                          <Tooltip title='The address of the participant must be a valid ethereum address.'>
-                            <InfoCircleOutlined
-                              style={{ color: 'rgba(0,0,0,.45)' }}
-                            />
-                          </Tooltip>
-                        }
-                      />
-                    </Form.Item>
-
-                    <Form.Item
-                      {...field}
-                      name={[field.name, 'partyTwitterHandle']}
-                      key={[field.key, 'partyTwitterHandle']}
-                      label={i === 0 ? 'Twitter username' : null}
-                      rules={[
-                        {
-                          pattern: /^@?[a-zA-Z0-9_]{1,15}$/,
-                          message: 'The username is not valid',
-                        },
-                        {
-                          min: 4,
-                          message:
-                            'Twitter handle must be at least 4 characters',
-                        },
-                        {
-                          max: 15,
-                          message:
-                            'Twitter handle must be less than 15 characters',
-                        },
-                      ]}
-                    >
-                      <Input
-                        placeholder='username'
-                        prefix={
-                          <span className='icon-double'>
-                            <i className='fa-brands fa-twitter'></i>
-                            <i className='fa-solid fa-at'></i>
-                          </span>
-                        }
-                        suffix={
-                          <Tooltip title='The twitter handle must be between 4 and 15 characters long. It is not required to provide one, but it is highly recommanded.'>
-                            <InfoCircleOutlined
-                              style={{ color: 'rgba(0,0,0,.45)' }}
-                            />
-                          </Tooltip>
-                        }
-                      />
-                    </Form.Item>
-
-                    <i
-                      className='fa-solid fa-trash'
-                      onClick={() => {
-                        // Prevent the user from removing the field if there is only one left
-                        if (fields.length > 1) {
-                          remove(field.name);
-                        } else {
-                          toast.warning('You need at least one participant');
-                        }
-                      }}
-                    ></i>
-                  </Space>
-                ))}
-                <Form.Item>
-                  <Button
-                    type='primary'
-                    disabled={submitLoading}
-                    onClick={() => {
-                      add();
-                    }}
-                    block
-                  >
-                    <i className='fa-solid fa-plus'></i> Add participant
-                  </Button>
-                </Form.Item>
-              </>
-            )}
-          </Form.List>
+          <NewParticipantList userAddress={userAddress} />
         </Form>
         upload pdf
       </div>
     </Drawer>
   );
 }
+
+const NewParticipantList = ({ userAddress }) => {
+  return (
+    <Form.List name='parties' initialValue={[{ partyAddress: userAddress }]}>
+      {(fields, { add, remove }) => (
+        <>
+          {fields.map((field, i) => (
+            <Space
+              key={field.key}
+              style={{ display: 'flex', marginBottom: 8 }}
+              align='center'
+            >
+              <Form.Item
+                {...field}
+                name={[field.name, 'partyName']}
+                key={[field.key, 'partyName']}
+                // Put a label only if it's the first field
+                label={i === 0 ? 'Name' : null}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Missing name',
+                  },
+                  {
+                    type: 'string',
+                    message: 'Name must be a string',
+                  },
+                  {
+                    min: 2,
+                    message: 'Name must be at least 2 characters',
+                  },
+                  {
+                    max: 30,
+                    message: 'Role must be less than 15 characters',
+                  },
+                ]}
+              >
+                <Input
+                  placeholder='John'
+                  prefix={<i className='fa-solid fa-user-tag'></i>}
+                  suffix={
+                    <Tooltip title='The name of the participant must be between 2 and 30 characters long.'>
+                      <InfoCircleOutlined
+                        style={{ color: 'rgba(0,0,0,.45)' }}
+                      />
+                    </Tooltip>
+                  }
+                />
+              </Form.Item>
+
+              <Form.Item
+                {...field}
+                name={[field.name, 'partyAddress']}
+                key={[field.key, 'partyAddress']}
+                label={i === 0 ? 'Ethereum address' : null}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Missing address',
+                  },
+                  {
+                    type: 'string',
+                    message: 'Address must be a number',
+                  },
+                  // Make sure it is an ethereum address
+                  {
+                    pattern: /^0x[a-fA-F0-9]{40}$/,
+                    min: 42,
+                    max: 42,
+                    message: 'Address must be a valid ethereum address',
+                  },
+                  // Make sure it's not the dead address
+                  {
+                    pattern:
+                      /^(?!.*?(0x0000000000000000000000000000000000000000)).*$/,
+                    message: 'You might want to use a different address...',
+                    warningOnly: true,
+                  },
+                ]}
+              >
+                <Input
+                  placeholder='0xc06...EF'
+                  prefix={<i className='fa-brands fa-ethereum'></i>}
+                  suffix={
+                    <Tooltip title='The address of the participant must be a valid ethereum address.'>
+                      <InfoCircleOutlined
+                        style={{ color: 'rgba(0,0,0,.45)' }}
+                      />
+                    </Tooltip>
+                  }
+                />
+              </Form.Item>
+
+              <Form.Item
+                {...field}
+                name={[field.name, 'partyTwitterHandle']}
+                key={[field.key, 'partyTwitterHandle']}
+                label={i === 0 ? 'Twitter username' : null}
+                rules={[
+                  {
+                    pattern: /^@?[a-zA-Z0-9_]{1,15}$/,
+                    message: 'The username is not valid',
+                  },
+                  {
+                    min: 4,
+                    message: 'Twitter handle must be at least 4 characters',
+                  },
+                  {
+                    max: 15,
+                    message: 'Twitter handle must be less than 15 characters',
+                  },
+                ]}
+              >
+                <Input
+                  placeholder='username'
+                  prefix={
+                    <span className='icon-double'>
+                      <i className='fa-brands fa-twitter'></i>
+                      <i className='fa-solid fa-at'></i>
+                    </span>
+                  }
+                  suffix={
+                    <Tooltip title='The twitter handle must be between 4 and 15 characters long. It is not required to provide one, but it is highly recommanded.'>
+                      <InfoCircleOutlined
+                        style={{ color: 'rgba(0,0,0,.45)' }}
+                      />
+                    </Tooltip>
+                  }
+                />
+              </Form.Item>
+
+              <i
+                className='fa-solid fa-trash'
+                onClick={() => {
+                  // Prevent the user from removing the field if there is only one left
+                  if (fields.length > 1) {
+                    remove(field.name);
+                  } else {
+                    toast.warning('You need at least one participant');
+                  }
+                }}
+              ></i>
+            </Space>
+          ))}
+          <Form.Item>
+            <Button
+              type='primary'
+              disabled={submitLoading}
+              onClick={() => {
+                add();
+              }}
+              block
+            >
+              <i className='fa-solid fa-plus'></i> Add participant
+            </Button>
+          </Form.Item>
+        </>
+      )}
+    </Form.List>
+  );
+};
