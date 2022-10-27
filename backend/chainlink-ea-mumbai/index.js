bfcf714982379b941245bee57473b99f6cbed9b6
polarzero <0xpolarzero@gmail.com>
2022-10-27 16:05:40 +0200

    Update request

--- backend/chainlink-ea-mumbai/index.js
+++ backend/chainlink-ea-mumbai/index.js
@@ -1,76 +1,92 @@
-const { Requester, Validator } = require('@chainlink/external-adapter')
+const { Requester, Validator } = require('@chainlink/external-adapter');
+require('dotenv').config();
 
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
 const customError = (data) => {
-  if (data.Response === 'Error') return true
-  return false
-}
+  if (data.Response === 'Error') return true;
+  return false;
+};
 
 // Define custom parameters to be used by the adapter.
 // Extra parameters can be stated in the extra object,
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  base: ['base', 'from', 'coin'],
-  quote: ['quote', 'to', 'market'],
-  endpoint: false
-}
+  username: ['username'],
+  endpoint: false,
+};
 
 const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
-  const validator = new Validator(callback, input, customParams)
-  const jobRunID = validator.validated.id
-  const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-  const fsym = validator.validated.data.base.toUpperCase()
-  const tsyms = validator.validated.data.quote.toUpperCase()
+  const validator = new Validator(callback, input, customParams);
+  const jobRunID = validator.validated.id;
+  const BEARER_TOKEN = process.env.BEARER_TOKEN;
+
+  const username = validator.validated.data.username;
+  // Add two urls: one for the user's profile and one for their latest tweets
+  const url = `https://api.twitter.com/2/users/by/username/${username}`;
 
   const params = {
-    fsym,
-    tsyms
-  }
+    username,
+  };
+
+  const headers = {
+    Authorization: `Bearer ${BEARER_TOKEN}`,
+  };
+
+  // * This works:
+  // curl \
+  // -H "Authorization: Bearer a2ZTYS13X3NiSTlwcnlfcHFGSG9QblBJd1UycFlVb2ZZZ3dDNUVFTzRhdkhvOjE2NjY4NzI3NTQ2Mjg6MTowOmF0OjE" \
+  // "https://api.twitter.com/2/users/by/username/TwitterDev"
+
+  // * But this won't work:
+  // curl -X POST -H "Content-Type: application/json" -d '{"id":"1","data":{"username":"TwitterDev"}}' http://localhost:8080
 
   // This is where you would add method and headers
   // you can add method like GET or POST and add it to the config
   // The default is GET requests
-  // method = 'get' 
+  // method = 'get'
   // headers = 'headers.....'
   const config = {
     url,
-    params
-  }
+    params,
+    headers,
+  };
 
   // The Requester allows API calls be retry in case of timeout
   // or connection failure
   Requester.request(config, customError)
-    .then(response => {
+    .then((response) => {
+      console.log(response);
       // It's common practice to store the desired value at the top-level
       // result key. This allows different adapters to be compatible with
       // one another.
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      callback(response.status, Requester.success(jobRunID, response))
+      // response.data.result = Requester.validateResultNumber(response.data, [
+      //   ['username'],
+      // ]);
+      callback(response.status, Requester.success(jobRunID, response));
     })
-    .catch(error => {
-      callback(500, Requester.errored(jobRunID, error))
-    })
-}
+    .catch((error) => {
+      callback(500, Requester.errored(jobRunID, error));
+    });
+};
 
 // This is a wrapper to allow the function to work with
 // GCP Functions
 exports.gcpservice = (req, res) => {
   createRequest(req.body, (statusCode, data) => {
-    res.status(statusCode).send(data)
-  })
-}
+    res.status(statusCode).send(data);
+  });
+};
 
 // This is a wrapper to allow the function to work with
 // AWS Lambda
 exports.handler = (event, context, callback) => {
   createRequest(event, (statusCode, data) => {
-    callback(null, data)
-  })
-}
+    callback(null, data);
+  });
+};
 
 // This is a wrapper to allow the function to work with
 // newer AWS Lambda implementations
@@ -79,11 +95,11 @@ exports.handlerv2 = (event, context, callback) => {
     callback(null, {
       statusCode: statusCode,
       body: JSON.stringify(data),
-      isBase64Encoded: false
-    })
-  })
-}
+      isBase64Encoded: false,
+    });
+  });
+};
 
 // This allows the function to be exported for testing
 // or for running in express
-module.exports.createRequest = createRequest
+module.exports.createRequest = createRequest;
