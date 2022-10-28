404de1eb559087ce21a18b4eabab35b43d60f16f
polarzero <0xpolarzero@gmail.com>
2022-10-28 17:07:28 +0200

    Init the Twitter Verification contract and change the job

--- backend/hardhat/hardhat.config.js
+++ backend/hardhat/hardhat.config.js
@@ -15,7 +15,11 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    compilers: [{ version: '0.8.17' }, { version: '0.6.6' }],
+    compilers: [
+      { version: '0.8.17' },
+      { version: '^0.8.7' },
+      { version: '0.6.6' },
+    ],
   },
   defaultNetwork: 'hardhat',
   networks: {
