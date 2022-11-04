ebdc074efae0826c3cbd3ba4ab748647ee643736
polarzero <0xpolarzero@gmail.com>
2022-11-04 22:58:14 +0100

    Add and customize a badge corresponding to IPFS nodes pinning the promise content

--- frontend/components/explore/IpfsResolver.js
+++ frontend/components/explore/IpfsResolver.js
@@ -1,10 +1,14 @@
 import IpfsDisplayPdf from './IpfsDisplayPdf';
 import IpfsDisplayDirectory from './IpfsDisplayDirectory';
+import FormattedAddress from '../utils/FormattedAddress';
 import { getContentFromCid } from '../../systems/getContentFromCid';
-import { Skeleton } from 'antd';
+import { getWeb3StorageClient } from '../../systems/getWeb3StorageClient';
+import { Badge, Popover, Skeleton } from 'antd';
 import { useEffect, useState } from 'react';
 
-export default function IpfsResolver({ uri }) {
+const web3StorageClient = getWeb3StorageClient();
+
+export default function IpfsResolver({ ipfsCid }) {
   const [resolvedData, setResolvedData] = useState({
     link: '',
     content: '',
@@ -14,7 +18,7 @@ export default function IpfsResolver({ uri }) {
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    getContentFromCid(uri)
+    getContentFromCid(ipfsCid)
       .then((data) => {
         if (data) {
           if (data.content === 'pdf') {
@@ -57,13 +61,111 @@ export default function IpfsResolver({ uri }) {
 
   if (isDirectory) {
     return (
-      <IpfsDisplayDirectory
-        link={resolvedData.link}
-        originalContent={resolvedData.content}
-        baseCid={resolvedData.baseCid}
-      />
+      <>
+        <IpfsDisplayDirectory
+          link={resolvedData.link}
+          originalContent={resolvedData.content}
+          baseCid={resolvedData.baseCid}
+        />
+        <IpfsPinning ipfsCid={ipfsCid} />
+      </>
     );
   }
 
-  return <IpfsDisplayPdf link={resolvedData.link} />;
+  return (
+    <>
+      <IpfsDisplayPdf link={resolvedData.link} />
+      <IpfsPinning ipfsCid={ipfsCid} />
+    </>
+  );
 }
+
+const IpfsPinning = ({ ipfsCid }) => {
+  const [ipfsPeers, setIpfsPeers] = useState([null]);
+  const [badgeColor, setBadgeColor] = useState('var(--toastify-color-warning)');
+
+  // Get the pinning status of the IPFS content
+  const getIpfsPinningStatus = async () => {
+    const info = await web3StorageClient.status(ipfsCid);
+    setIpfsPeers(info.pins);
+  };
+
+  useEffect(() => {
+    getIpfsPinningStatus();
+  }, []);
+
+  useEffect(() => {
+    if (ipfsPeers.length <= 3) {
+      setBadgeColor('var(--toastify-color-warning)');
+    } else {
+      setBadgeColor('var(--toastify-color-success)');
+    }
+  }, [ipfsPeers]);
+
+  if (!ipfsPeers || ipfsPeers.length === 0 || ipfsPeers[0] === null) {
+    return (
+      <div className='security'>
+        <Skeleton
+          active
+          paragraph={false}
+          avatar={{ size: 'small' }}
+          title={false}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className='security'>
+      <div className='ipfs-pins'>
+        <Popover
+          content={
+            <div>
+              This IPFS content is currently being pinned by{' '}
+              <b>{ipfsPeers.length} peers</b>. <br />
+              The more peers that pin the content, the more secure it is. <br />
+              If you want to pin the content yourself, and make it un-deletable,
+              you can: <br />
+              <a
+                href='https://docs.ipfs.tech/how-to/companion-node-types/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                - follow this guide to run your own IPFS node
+              </a>
+              <br />- use the IPFS CID as the content to pin:{' '}
+              <FormattedAddress
+                address={ipfsCid}
+                isShrinked={true}
+                type='ipfs'
+              />
+              <br />
+              <br />
+              Current peers pinning this content:
+              <br />
+              {ipfsPeers.map((peer) => (
+                <>
+                  <FormattedAddress
+                    key={peer.peerId}
+                    address={peer.peerId}
+                    isShrinked={true}
+                    type='ipfs'
+                  />
+                  <br />
+                </>
+              ))}
+            </div>
+          }
+        >
+          <Badge
+            count={ipfsPeers.length}
+            color={badgeColor}
+            style={{ marginRight: '-0.5rem' }}
+          >
+            <i className='fas fa-sitemap' />
+          </Badge>
+        </Popover>
+      </div>
+    </div>
+  );
+};
--- frontend/components/explore/PromisePanel.js
+++ frontend/components/explore/PromisePanel.js
@@ -5,21 +5,14 @@ import {
   getPartiesApprovedStatus,
   getPartiesTwitterVerifiedStatus,
 } from '../../systems/promisePartiesData';
-import { getWeb3StorageClient } from '../../systems/getWeb3StorageClient';
 import { useProvider, useNetwork } from 'wagmi';
 import { useEffect, useState } from 'react';
-import { Popover, Skeleton } from 'antd';
-import FormattedAddress from '../utils/FormattedAddress';
-
-const web3StorageClient = getWeb3StorageClient();
 
 export default function PromisePanel({ contractAttributes }) {
   const [isPromiseLocked, setIsPromiseLocked] = useState(null);
   const [addressToApprovedStatus, setAddressToApprovedStatus] = useState([]);
   const [addressToTwitterVerifiedStatus, setAddressToTwitterVerifiedStatus] =
     useState([]);
-  const [isIpfsLoading, setIsIpfsLoading] = useState(true);
-  const [ipfsPeers, setIpfsPeers] = useState([null]);
   const provider = useProvider();
   const { chain } = useNetwork();
 
@@ -55,52 +48,20 @@ export default function PromisePanel({ contractAttributes }) {
     setAddressToTwitterVerifiedStatus(partiesTwitterVerifiedStatus);
   };
 
-  // Get the pinning status of the IPFS content
-  const getIpfsPinningStatus = async () => {
-    const info = await web3StorageClient.status(ipfsCid);
-    setIpfsPeers(info.pins);
-    setIsIpfsLoading(false);
-  };
-
   useEffect(() => {
     gatherPartiesData();
-    getIpfsPinningStatus();
   }, []);
 
   return (
     <div className='promise-card'>
-      <div className='security'>
-        {isIpfsLoading ? (
-          <Skeleton active paragraph={{ rows: 1 }} title={false} />
-        ) : (
-          <div className='ipfs-pins'>
-            Pinned by {ipfsPeers.length} IPFS nodes
-            <Popover
-              content={ipfsPeers.map((peer) => (
-                <p>
-                  {
-                    <FormattedAddress
-                      address={peer.peerId}
-                      isShrinked={true}
-                      type='ipfs'
-                    />
-                  }
-                </p>
-              ))}
-            >
-              <i className='fas fa-info-circle' />
-            </Popover>
-          </div>
-        )}
-      </div>
       <PromiseTable
         contractAttributes={contractAttributes}
         isPromiseLocked={isPromiseLocked}
         addressToApprovedStatus={addressToApprovedStatus}
         addressToTwitterVerifiedStatus={addressToTwitterVerifiedStatus}
       />
-      <div key='viewer' className='card-item pdf-viewer'>
-        <IpfsResolver uri={ipfsCid} />
+      <div key='viewer' className='card-item ipfs-viewer'>
+        <IpfsResolver ipfsCid={ipfsCid} />
       </div>
     </div>
   );
