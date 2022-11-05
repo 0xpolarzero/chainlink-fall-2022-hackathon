8912dafd7c8b9715b738a1c2ae72d2953056e346
polarzero <0xpolarzero@gmail.com>
2022-11-05 11:37:25 +0100

    Display title and Tooltip

--- frontend/components/explore/IpfsDisplayPdf.js
+++ frontend/components/explore/IpfsDisplayPdf.js
@@ -1,5 +1,6 @@
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { toolbarPlugin } from '@react-pdf-viewer/toolbar';
+import { Tooltip } from 'antd';
 import { useEffect, useState } from 'react';
 
 export default function IpfsDisplayPdf({ link }) {
@@ -22,42 +23,79 @@ export default function IpfsDisplayPdf({ link }) {
 
   try {
     return (
-      <Worker workerUrl='https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.js'>
-        <div
-          className='rpv-core__viewer'
-          style={{
-            border: '1px solid rgba(0, 0, 0, 0.3)',
-            display: 'flex',
-            flexDirection: 'column',
-            // Fill the available space in the parent but don't overflow
-            height: `${availableHeight}px`,
-          }}
-        >
-          <div
-            style={{
-              alignItems: 'center',
-              backgroundColor: '#eeeeee',
-              borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
-              display: 'flex',
-              padding: '0.25rem',
-            }}
-          >
-            <Toolbar>{renderDefaultToolbar(transform)}</Toolbar>;
-          </div>
-          <div
-            style={{
-              flex: 1,
-              overflow: 'hidden',
-            }}
-          >
-            <Viewer
-              fileUrl={link}
-              theme='light'
-              plugins={[toolbarPluginInstance]}
-            />
-          </div>
+      <>
+        <div className='ant-table-title'>
+          <b>
+            IPFS directory{' '}
+            <Tooltip
+              title={
+                <span>
+                  <p>
+                    The content of the IPFS CID that was provided when creating
+                    the promise. If done through the UI, the storage is provided
+                    by{' '}
+                    <a
+                      href='https://blog.web3.storage/posts/say-hello-to-the-data-layer-1-3-intro-to-web3-storage'
+                      target='_blank'
+                    >
+                      Web3.Storage
+                    </a>
+                    , leveraging the Filecoin persistent long-term storage
+                    solution. If done directly through the Smart Contract, the
+                    user can provide any valid CID.
+                  </p>
+                  <p>
+                    <b>NOTE:</b> As soon as the promise is locked, the contract
+                    will try to perform a permanent backup to{' '}
+                    <a href='https://arwiki.wiki/#/en/main' target='_blank'>
+                      Arweave
+                    </a>
+                    , the result of which will be displayed on the badge.
+                  </p>
+                </span>
+              }
+            >
+              <i className='fas fa-question-circle' />
+            </Tooltip>
+          </b>
         </div>
-      </Worker>
+        <Worker workerUrl='https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.js'>
+          <div
+            className='rpv-core__viewer'
+            style={{
+              border: '1px solid rgba(0, 0, 0, 0.3)',
+              display: 'flex',
+              flexDirection: 'column',
+              // Fill the available space in the parent but don't overflow
+              height: `${availableHeight}px`,
+            }}
+          >
+            <div
+              style={{
+                alignItems: 'center',
+                backgroundColor: '#eeeeee',
+                borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                display: 'flex',
+                padding: '0.25rem',
+              }}
+            >
+              <Toolbar>{renderDefaultToolbar(transform)}</Toolbar>;
+            </div>
+            <div
+              style={{
+                flex: 1,
+                overflow: 'hidden',
+              }}
+            >
+              <Viewer
+                fileUrl={link}
+                theme='light'
+                plugins={[toolbarPluginInstance]}
+              />
+            </div>
+          </div>
+        </Worker>
+      </>
     );
   } catch (error) {
     console.log(error);
