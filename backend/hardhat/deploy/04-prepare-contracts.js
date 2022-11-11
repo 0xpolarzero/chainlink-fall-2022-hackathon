8fd4598bf102a5d997b32134c1db97ada6032238
polarzero <0xpolarzero@gmail.com>
2022-11-11 20:40:30 +0100

    Add 'prepare' deployment to setup verifiers & fund them

---
+++ backend/hardhat/deploy/04-prepare-contracts.js
@@ -0,0 +1,45 @@
+const { network, ethers } = require('hardhat');
+const { LINK_TOKEN_MUMBAI } = require('../helper-hardhat-config');
+
+const LINK_FUNDING_AMOUNT = ethers.utils.parseEther('2');
+
+module.exports = async function({ getNamedAccounts, deployments }) {
+  const { deployer } = await getNamedAccounts();
+  let promiseFactory = await deployments.get('PromiseFactory');
+  let verifyTwitter = await deployments.get('VerifyTwitter');
+  let verifyStorage = await deployments.get('VerifyStorage');
+
+  promiseFactory = await ethers.getContractAt(
+    'PromiseFactory',
+    promiseFactory.address,
+    deployer,
+  );
+  verifyTwitter = await ethers.getContractAt(
+    'VerifyTwitter',
+    verifyTwitter.address,
+    deployer,
+  );
+  verifyStorage = await ethers.getContractAt(
+    'VerifyStorage',
+    verifyStorage.address,
+    deployer,
+  );
+
+  // Set verifiers
+  console.log('Setting allowed verifiers...');
+  await promiseFactory.setTwitterVerifier(verifyTwitter.address);
+  await promiseFactory.setStorageVerifier(verifyStorage.address);
+
+  // Fund contracts with Link
+  const linkToken = await ethers.getContractAt(
+    '@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol:LinkTokenInterface',
+    LINK_TOKEN_MUMBAI,
+    deployer,
+  );
+  console.log('Funding VerifyTwitter contract with LINK...');
+  await linkToken.transfer(verifyTwitter.address, LINK_FUNDING_AMOUNT);
+  console.log('Funding VerifyStorage contract with LINK...');
+  await linkToken.transfer(verifyStorage.address, LINK_FUNDING_AMOUNT);
+};
+
+module.exports.tags = ['all', 'prepare', 'main'];
