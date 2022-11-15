6fb9ab117efb10b80bb60075b1a1287f23d1c1d5
polarzero <0xpolarzero@gmail.com>
2022-11-15 16:11:42 +0100

    Add FAQ & links to home

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,47 @@
 import Link from 'next/link';
 import ParticleLogo from '../components/home/ParticleLogo';
-import { Button } from 'antd';
+import { Button, Card, Divider } from 'antd';
+
+const gridStyle = {
+  minWidth: '200px',
+  textAlign: 'center',
+  alignSelf: 'center',
+  height: '100%',
+};
 
 export default function Home() {
+  const faqLinks = [
+    {
+      title: 'Understanding a promise',
+      path: 'how-to-use/exploring-promises',
+    },
+    {
+      title: 'Creating a promise',
+      path: 'how-to-use/creating-a-promise',
+    },
+    {
+      title: 'Interacting with your promises',
+      path: 'how-to-use/interacting-with-promises',
+    },
+    {
+      title: 'Verifying your Twitter account',
+      path: 'how-to-use/verifying-a-twitter-account',
+    },
+    {
+      title: 'How is the Twitter verification performed?',
+      path: 'chainlink-external-adapters/twitter-account-verification',
+    },
+    {
+      title:
+        'How are the persistance and integrity of a promise content ensured?',
+      path: 'chainlink-external-adapters/ipfs-and-arweave-verification',
+    },
+  ];
+
+  const goToDocumentation = (path) => {
+    window.open(`https://docs.usepromise.xyz/${path}`, '_blank').focus();
+  };
+
   return (
     <>
       <main className='home'>
@@ -40,6 +79,29 @@ export default function Home() {
                   Explore verified Twitter handles
                 </Button>
               </Link>
+            </div>
+          </div>
+        </section>
+        <Divider />
+        <section className='section section-about'>
+          <div className='faq'>
+            <div className='header'>
+              <div className='title'>How to use</div>
+              <div className='caption'>Useful links to get started</div>
+            </div>
+
+            <div className='links-list'>
+              <Card title={false} bordered={false}>
+                {faqLinks.map((link, index) => (
+                  <Card.Grid
+                    key={index}
+                    style={gridStyle}
+                    onClick={() => goToDocumentation(link.path)}
+                  >
+                    {link.title}
+                  </Card.Grid>
+                ))}
+              </Card>
             </div>
           </div>
         </section>
