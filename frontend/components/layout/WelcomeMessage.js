57d2bb58056a82eafece2696373c18384f1bb66d
polarzero <0xpolarzero@gmail.com>
2022-11-16 11:06:45 +0100

    Update WelcomeMessage.js

--- frontend/components/layout/WelcomeMessage.js
+++ frontend/components/layout/WelcomeMessage.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 export default function WelcomeMessage() {
   const message = (
     <>
-      Please note that along with currently deploying on tesnet, we are using a{' '}
+      Please note that while operating on a tesnet, we are using a{' '}
       <b>Bundlr devnet node</b>.
       <br />
       <br /> This means that files are actually never moved to Arweave, and are{' '}
