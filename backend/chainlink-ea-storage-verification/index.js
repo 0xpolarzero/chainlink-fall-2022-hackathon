c3ed54c24b1c9ee8087d282bca394c959eaadd20
polarzero <0xpolarzero@gmail.com>
2022-11-17 12:16:02 +0100

    Improve error handling

--- backend/chainlink-ea-storage-verification/index.js
+++ backend/chainlink-ea-storage-verification/index.js
@@ -42,22 +42,28 @@ const createRequest = (input, callback) => {
       'base64',
     );
 
-    // Decrypt it
-    const decryptedData = CryptoJS.AES.decrypt(encryptedBase64, key, {
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
-    });
-    // We must transform to lowercase because the addresses can mismatch if not
-    const decryptedString = decryptedData
-      .toString(CryptoJS.enc.Utf8)
-      .toLowerCase();
+    // We're using a nested try/catch here because otherwise the decrypt function
+    // does sometimes throw an error when the key is wrong
+    // In this case, we want to return a 200 response with a status of 1
+    let decryptedString;
+    try {
+      // Decrypt it
+      const decryptedData = CryptoJS.AES.decrypt(encryptedBase64, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7,
+      });
+      // We must transform to lowercase because the addresses can mismatch if not
+      decryptedString = decryptedData.toString(CryptoJS.enc.Utf8).toLowerCase();
+    } catch (err) {
+      decryptedString = '';
+    }
+
     const expectedString = (userAddress + ipfsCid + arweaveId).toLowerCase();
 
     // If the strings don't match, return 1
     // If they do and arweaveId is empty, return 2 (not been uploaded to Arweave)
     // If they do and an arweaveId is provided, return 3
-
     const storageStatus =
       decryptedString === expectedString ? (arweaveId ? 3 : 2) : 1;
 
